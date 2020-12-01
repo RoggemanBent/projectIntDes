6587cecb1cdac7f6231534d611180e4dc241c520
Bent Roggeman <bent.roggeman@student.howest.be>
2020-12-01 14:47:04 +0100

    Pokedex_page_v1 finished

--- script/app.js
+++ script/app.js
@@ -1,51 +1,149 @@
-// _ = helper functions
-function _parseMillisecondsIntoReadableTime(timestamp) {
-	//Get hours from milliseconds
-	const date = new Date(timestamp * 1000);
-	// Hours part from the timestamp
-	const hours = '0' + date.getHours();
-	// Minutes part from the timestamp
-	const minutes = '0' + date.getMinutes();
-	// Seconds part from the timestamp (gebruiken we nu niet)
-	// const seconds = '0' + date.getSeconds();
+const poke_container = document.getElementById('poke_container');
+const pokemons_number = 151;
+var backgroundType1;
+var backgroundType2;
+var firstType = "";
+var secondType = "";
+const colors = {
+	normal: '#BDBDAE',
+	poison: '#A15999',
+	psychic: '#FA65B6',
+	grass: '#8BD650',
+	ground: '#ECCC57',
+	ice: '#96F1FF',
+	fire: '#FA5543',
+	rock: '#CEBD72',
+	dragon: '#8874FF',
+	water: '#56AEFF',
+	bug: '#C2D21F',
+	dark: '#886653',
+	fighting: '#A75545',
+	ghost: '#7975D7',
+	steel: '#C4C2DB',
+	flying: '#79A4FF',
+	electric: '#FDE53C',
+	fairy: '#F9ADFF',
+};
+const main_types = Object.keys(colors);
 
-	// Will display time in 10:30(:23) format
-	return hours.substr(-2) + ':' + minutes.substr(-2); //  + ':' + s
+const fetchPokemons = async () => {
+    for (let i = 1; i <= pokemons_number; i++) {
+        await getPokemon(i);
+    }
 }
 
-// 5 TODO: maak updateSun functie
-
-// 4 Zet de zon op de juiste plaats en zorg ervoor dat dit iedere minuut gebeurt.
-let placeSunAndStartMoving = (totalMinutes, sunrise) => {
-	// In de functie moeten we eerst wat zaken ophalen en berekenen.
-	// Haal het DOM element van onze zon op en van onze aantal minuten resterend deze dag.
-	// Bepaal het aantal minuten dat de zon al op is.
-	// Nu zetten we de zon op de initiële goede positie ( met de functie updateSun ). Bereken hiervoor hoeveel procent er van de totale zon-tijd al voorbij is.
-	// We voegen ook de 'is-loaded' class toe aan de body-tag.
-	// Vergeet niet om het resterende aantal minuten in te vullen.
-	// Nu maken we een functie die de zon elke minuut zal updaten
-	// Bekijk of de zon niet nog onder of reeds onder is
-	// Anders kunnen we huidige waarden evalueren en de zon updaten via de updateSun functie.
-	// PS.: vergeet weer niet om het resterend aantal minuten te updaten en verhoog het aantal verstreken minuten.
-};
-
-// 3 Met de data van de API kunnen we de app opvullen
-let showResult = queryResponse => {
-	// We gaan eerst een paar onderdelen opvullen
-	// Zorg dat de juiste locatie weergegeven wordt, volgens wat je uit de API terug krijgt.
-	// Toon ook de juiste tijd voor de opkomst van de zon en de zonsondergang.
-	// Hier gaan we een functie oproepen die de zon een bepaalde positie kan geven en dit kan updaten.
-	// Geef deze functie de periode tussen sunrise en sunset mee en het tijdstip van sunrise.
-};
-
-// 2 Aan de hand van een longitude en latitude gaan we de yahoo wheater API ophalen.
-let getAPI = (lat, lon) => {
-	// Eerst bouwen we onze url op
+const getPokemon = async (id) => {
 	// Met de fetch API proberen we de data op te halen.
+	const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+		.then(r=>r.json())
+		.catch((err)=>console.error("error: ", err));
+		
 	// Als dat gelukt is, gaan we naar onze showResult functie.
+	// console.log(pokemon);
+	createPokemonCard(pokemon);
 };
+
+function createPokemonCard(pokemon) {
+    const pokemonEl = document.createElement('div');
+    pokemonEl.classList.add('pokemon');
+
+	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+	firstType = pokemon.types[0].type.name;
+	
+	if (pokemon.types.length > 1){
+		secondType = pokemon.types[1].type.name;
+	}
+
+	for (const color in colors){
+		if (firstType == color) {
+			backgroundType1 = colors[color];
+			console.log(backgroundType1);
+		}
+		if (secondType == color) {
+			backgroundType2 = colors[color];
+			console.log(backgroundType2);
+		}
+	}
+
+	const pokeInnerHTML = `
+	<h2 class="pokemon">
+		#${pokemon.id.toString().padStart(3, '0')} ${name}
+	</h2>	
+	<div class="c-pokedex">
+		<div class="grid_photo"> 
+			<div class="c-pokedex__item" style="background-color: ${backgroundType1}">
+				<div class="c-card">
+					<div class="c-card__body">
+						<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png">
+					</div>                     
+				</div>
+			</div>
+		</div>
+		<div class="grid_type"> 
+			<div class="c-pokedex__item">
+				<div class="c-card">
+					<div class="c-card__body">
+						<h3>Types</h3>
+						<div class="row">
+							<div class="column">
+							  <p class="type" style="background-color: ${backgroundType1};">${pokemon.types[0].type.name}</p>
+							  </div>
+							  <div class="column">
+							  <p class="type" style="background-color: ${backgroundType2};">${secondType}</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="grid_stats"> 
+			<div class="c-pokedex__item">
+				<div class="c-card">
+					<div class="c-card__body">
+						<h3>Stats</h3>
+                        <label class="label" for="file">HP</label>
+                        <progress id="file" value="${pokemon.stats[0].base_stat}" max="100"></progress>
+                        <label class="label" for="file">Attack</label>
+                        <progress id="file" value="${pokemon.stats[1].base_stat}" max="100"></progress>
+                        <label class="label" for="file">Defense</label>
+                        <progress id="file" value="${pokemon.stats[2].base_stat}" max="100"></progress>
+                        <label class="label" for="file">Speed</label>
+                        <progress id="file" value="${pokemon.stats[3].base_stat}" max="100"></progress>
+                        <label class="label" for="file">Special Attack</label>
+                        <progress id="file" value="${pokemon.stats[4].base_stat}" max="100"></progress>
+                        <label class="label" for="file">Special Defense</label>
+                        <progress id="file" value="${pokemon.stats[5].base_stat}" max="100"></progress>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="grid_profile"> 
+			<div class="c-pokedex__item">
+				<div class="c-card">
+					<div class="c-card__body">
+						<h3>Profile</h3>
+						<div class="row">
+							<div class="column">
+							  <p>Height: ${(pokemon.height)/10} m</p>
+							  </div>
+							  <div class="column">
+							  <p>Weight: ${(pokemon.weight)/10} kg</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    `;
+
+    pokemonEl.innerHTML = pokeInnerHTML;
+
+	poke_container.appendChild(pokemonEl);
+	secondType = "";
+}
 
 document.addEventListener('DOMContentLoaded', function() {
-	// 1 We will query the API with longitude and latitude.
-	getAPI(50.8027841, 3.2097454);
+	fetchPokemons();
 });
